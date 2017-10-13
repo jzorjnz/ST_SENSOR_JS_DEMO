f4872c4328c727be0cbc5848e3b616e598e48c0d
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2017-10-13 09:07:10 +0200

    Added LSM303AGR Sensor demo

--- mbedos5/js/main.js
+++ mbedos5/js/main.js
@@ -25,8 +25,11 @@ var count = 0;
 var hts221 = null;
 var lps22hb = null;
 var lsm6dsl = null;
+var lsm303agr = null;
 var acc = null;
 var gyr = null;
+var acc2 = null;
+var mag2 = null;
 
 var iv = setInterval(function() {
     led1.write(led1.read()? 0: 1);
@@ -45,6 +48,10 @@ var iv = setInterval(function() {
         lsm6dsl = LSM6DSL_JS(i2c);
         print("Loading complete.");
         
+        print("Loading LSM303AGR sensor...");
+        lsm303agr = LSM303AGR_JS(i2c);
+        print("Loading complete.");
+        
         hts221.led_on(led2);
     }
     else if(count < 2){
@@ -59,8 +66,14 @@ var iv = setInterval(function() {
         acc = JSON.parse(lsm6dsl.get_accelerometer_axes());
         gyr = JSON.parse(lsm6dsl.get_gyroscope_axes());
 
+        acc2 = JSON.parse(lsm303agr.get_accelerometer_axes());
+        mag2 = JSON.parse(lsm303agr.get_magnetometer_axes());
+        
         print("LSM6DSL: [Gyroscope]: " + gyr.x + ", " + gyr.y + ", " + gyr.z + 
         "   [Accelerometer]: " + acc.x + ", " + acc.y + ", " + acc.z);
+        
+        print("LSM303AGR: [Magnetometer]: " + mag2.x + ", " + mag2.y + ", " + mag2.z + 
+        "   [Accelerometer]: " + acc2.x + ", " + acc2.y + ", " + acc2.z);
         
         print("");
     }
